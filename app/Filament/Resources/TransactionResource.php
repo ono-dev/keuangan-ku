f52ec2a73a2a2d07b4a4ab1b5c8ee269a3ca2477
ono dev <nyacakinyacaki@gmail.com>
2024-09-22 22:58:15 +0700

    add: Dashboard Admin panel

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -30,7 +30,7 @@ class TransactionResource extends Resource
                 Forms\Components\Select::make('category_id')
                 ->relationship('category', 'name')
                     ->required(),
-                Forms\Components\DatePicker::make('date')
+                Forms\Components\DatePicker::make('date_transaction')
                     ->required(),
                 Forms\Components\TextInput::make('amount')
                     ->required()
@@ -63,7 +63,7 @@ class TransactionResource extends Resource
                     ->falseColor('success')
                     ->boolean(),
                 
-                Tables\Columns\TextColumn::make('date')
+                Tables\Columns\TextColumn::make('date_transaction')
                 ->label('Tanggal')
                     ->sortable(),
                 Tables\Columns\TextColumn::make('amount')
--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -5,13 +5,24 @@ namespace App\Filament\Widgets;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use App\Models\Transaction;
+use Illuminate\Support\Carbon;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 
 class StatsOverview extends BaseWidget
 {
+    use InteractsWithPageFilters;
     protected function getStats(): array
     {
-        $incomes = Transaction::incomes()->get()->sum('amount');
-        $outcomes = Transaction::expenses()->get()->sum('amount');
+        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
+            Carbon::parse($this->filters['startDate']) :
+            null;
+
+        $endDate = ! is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now();
+
+        $incomes = Transaction::incomes()->whereBetween('date_transaction', [$startDate, $endDate])->sum('amount');
+        $outcomes = Transaction::expenses()->whereBetween('date_transaction', [$startDate, $endDate])->sum('amount');
         $diference = $incomes - $outcomes;
         // $outcomes = Transaction::join('categories', 'categories.id', '=', 'transactions.category_id')
         // ->where('categories.is_expense', true);
